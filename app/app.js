a8612ed480dba38e0804050fbef7b80ec86cbc96
deekshithpranav <deekshithpranav@gmail.com>
2022-07-13 11:06:25 +0530

    modified

--- app/app.js
+++ app/app.js
@@ -3,8 +3,10 @@ document.getElementById('login').addEventListener('click', loginWithFacebook, fa
 function loginWithFacebook() {
     let status = 0;
     let url = "";
+    let appId = 510618090830554;
+    let redirect_uri = "http://localhost:3000/test.html";
     if(status == 0){
-        window.location.replace(`https://www.facebook.com/v14.0/dialog/oauth?client_id=510618090830554&redirect_uri=http://localhost:3000/test.html&state=5896245`);
+        window.location.replace(`https://www.facebook.com/v14.0/dialog/oauth?client_id=${appId}&redirect_uri=${redirect_uri}&state=5896245`);
         url = window.location.href;
         console.log(url)
     }
